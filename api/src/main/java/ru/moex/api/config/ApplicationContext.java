f95537f46fbb0d5a1374b4cb04cec4a4531473f8
levin.a <Aleksey.Levin@moex.com>
2017-05-17 17:02:14 +0300

    static import constant, fix bug, refactoring

--- api/src/main/java/ru/moex/api/config/ApplicationContext.java
+++ api/src/main/java/ru/moex/api/config/ApplicationContext.java
@@ -1,11 +1,15 @@
 package ru.moex.api.config;
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
+import static ru.moex.api.config.ApplicationContext.BASE_PACKAGE;
+
 @Configuration
-@ComponentScan(basePackages = "ru.moex.api", includeFilters = @ComponentScan.Filter({Service.class, Repository.class}))
+@ComponentScan(basePackages = BASE_PACKAGE, includeFilters = @Filter({Service.class, Repository.class}))
 public class ApplicationContext {
+    public static final String BASE_PACKAGE = "ru.moex.api";
 }
